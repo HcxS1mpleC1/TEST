6d882e2ec4f5af34655c925d9ee4efa3499f1fcb
HermanHu <110174639+HcxS1mpleC1@users.noreply.github.com>
2022-08-07 10:54:30 +0800

    Add files via upload

---
+++ Test1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <windows.h>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	int* H = NULL;
+	int G = 0;
+
+	while (true)
+	{
+
+		int* L = new int(0);
+		
+		//L=&G;
+		//if (*H = NULL)
+		//{
+			//return 1;
+		//}
+		
+	}
+
+	return 0;
+}
